55037a1a8c6f1d277c67f9182692dab50eee6d27
Okada Shohei <okashoicircus0@gmail.com>
2020-08-30 13:34:21 +0900

    デプロイシェルいった

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -eu
+
+# アプリケーションのビルド
+cd /home/isucon/isubata/webapp/go
+make
+
+# 各種サービス停止
+sudo systemctl stop nginx
+sudo systemctl stop isubata.golang.service
+sudo systemctl stop mysql
+
+# ログのクリーンアップ
+echo ''  | sudo tee /var/log/mysql/mysql-slow.log /var/log/nginx/access.log
+
+# 各種サービス起動
+sudo systemctl stop mysql
+sudo systemctl stop isubata.golang.service
+sudo systemctl stop nginx
+
+# slow query log の設定
+mysql -uisucon -pisucon << EOF
+set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';
+set global long_query_time = 0.1;
+set global slow_query_log = ON;
+set global log_queries_not_using_indexes = ON;
+EOF
